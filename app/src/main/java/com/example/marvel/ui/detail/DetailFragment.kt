4151f9edec7b4b1c477e600f003ad1cf79c07f90
teo <teofilo.gonzalez@kairosds.com>
2022-06-27 15:29:09 +0200

    Add -> detail delegate

--- app/src/main/java/com/example/marvel/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/marvel/ui/detail/DetailFragment.kt
@@ -1,5 +1,6 @@
 package com.example.marvel.ui.detail
 
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
@@ -8,24 +9,37 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.marvel.R
 import com.example.marvel.base.Base
 import com.example.marvel.databinding.FragmentDetailBinding
+import com.example.marvel.repository.model.ApiError
+import com.example.marvel.repository.model.DetailResponse
 import com.example.marvel.repository.model.ResultsItem
+import com.example.marvel.utils.Common.gone
 
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class DetailFragment : Base.BaseFragment<FragmentDetailBinding>(R.layout.fragment_detail) {
+class DetailFragment : Base.BaseFragment<FragmentDetailBinding>(R.layout.fragment_detail),
+    DetailViewModelDelegate {
 
     private val detailViewModel: DetailViewModel by viewModels()
 
     override fun init() {
+        detailViewModel.delegate = this
         arguments?.getString("id")?.let { it ->
-            detailViewModel.getDetail(it).observe(this) { detail ->
-                detail?.let {
-                    setup(detail.data?.results?.get(0))
-                }
-            }
+            detailViewModel.getDetail(it)
         }
+    }
+
+    override fun onSuccess(detail: DetailResponse?) {
+        binding.progressBarDetail.gone()
+        detail?.let {
+            setup(detail.data?.results?.get(0))
+        }
+    }
+
+    override fun onFailure(error: ApiError) {
+        binding.progressBarDetail.gone()
+        Toast.makeText(requireContext(), error.body, Toast.LENGTH_LONG).show()
     }
 
     private fun setup(result: ResultsItem?) {
--- app/src/main/java/com/example/marvel/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/marvel/ui/detail/DetailViewModel.kt
@@ -1,7 +1,7 @@
 package com.example.marvel.ui.detail
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.marvel.repository.model.ApiError
 import com.example.marvel.repository.model.DetailResponse
 import com.example.marvel.repository.service.MarvelRepository
 import com.example.marvel.repository.service.MarvelService
@@ -9,27 +9,22 @@ import com.example.marvel.repository.service.MarvelServiceImpl
 import com.example.marvel.utils.Common.getHash
 import com.example.marvel.utils.Common.publicKey
 import com.example.marvel.utils.Common.ts
-import retrofit2.Response
 
 class DetailViewModel : ViewModel() {
 
+    var delegate: DetailViewModelDelegate? = null
     private val marvelRepository: MarvelRepository = MarvelServiceImpl()
 
-    fun getDetail(id: String) : MutableLiveData<DetailResponse> {
-
-        val liveData: MutableLiveData<DetailResponse> = MutableLiveData()
-
+    fun getDetail(id: String) {
         marvelRepository.getDetail(id, ts, publicKey, getHash(),
             object : MarvelService.CallbackResponse<DetailResponse> {
                 override fun onResponse(response: DetailResponse) {
-                    liveData.postValue(response)
+                    delegate?.onSuccess(response)
                 }
 
-                override fun onFailure(t: Throwable, res: Response<*>?) {
-                    liveData.postValue(null)
+                override fun onFailure(error: ApiError) {
+                    delegate?.onFailure(error)
                 }
             })
-
-        return liveData
     }
 }
